c0b0b31a42211a99424c14c460cb238b98c6ee4a
Jan Steikowski <j.steikowski@aol.com>
2023-10-20 15:04:05 +0700

    Add env variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
+      <p>{import.meta.env.VITE_APP_TITLE}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
