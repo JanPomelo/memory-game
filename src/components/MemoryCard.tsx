97b1456a9735c079a51b945192285aa16db5b451
Jan Steikowski <j.steikowski@aol.com>
2023-10-23 11:31:42 +0700

    Add all drivers and tested the apiCall for all

--- src/components/MemoryCard.tsx
+++ src/components/MemoryCard.tsx
@@ -1,7 +1,8 @@
 "use strict";
 
 import { Driver } from "../types";
-//import { getDriverDetails } from "../apiCalls";
+import { getDriverDetails } from "../apiCalls";
+import { drivers } from "../drivers";
 
 const albon: Driver = {
   strNumber: "23",
@@ -11,26 +12,41 @@ const albon: Driver = {
   strCutout: "https://www.thesportsdb.com/images/media/player/cutout/2f73pc1679830795.png",
 };
 
+async function getAllDetails(driverArr: { firstName: string; lastName: string }[]) {
+  const allDriverDetails: Driver[] = [];
+  for (let i = 0; i < driverArr.length; i++) {
+    //const driverInfos = await getDriverDetails({ firstName: driverArr[i].firstName, lastName: driverArr[i].lastName });
+    //allDriverDetails.push(driverInfos);
+  }
+  allDriverDetails.push(albon);
+  return allDriverDetails;
+}
 
-//const albon = await getDriverDetails({firstName: 'Max', lastName: 'Verstappen'}) as Driver;
+const allDetails = await getAllDetails(drivers);
 
 export default function MemoryCard() {
   return (
-    <div className="outerMemCard">
-      <div className="memCard">
-        <div className="imgWrapper">
-          <div className="f1Logo"></div>
-          <img src={albon.strCutout} />
-          <div className="imgGradient1"></div>
-          <div className="imgGradient2"></div>
-        </div>
-        <hr></hr>
-        <h3 className="fNameDriver">{albon.strPlayer.substring(0, albon.strPlayer.indexOf(' '))}</h3>
-        <h3 className="lNameDriver">{albon.strPlayer.substring(albon.strPlayer.indexOf(' '))}</h3>
-        <div className="driversNameBG1"></div>
-        <div className="driversNameBG2"></div>
-        <p className="driverNumber">{albon.strNumber}</p>
-      </div>
-    </div>
+    <>
+      {allDetails.map((driver) => {
+        return (
+          <div className="outerMemCard">
+            <div className="memCard">
+              <div className="imgWrapper">
+                <div className="f1Logo"></div>
+                <img src={driver?.strCutout} />
+                <div className="imgGradient1"></div>
+                <div className="imgGradient2"></div>
+              </div>
+              <hr></hr>
+              <h3 className="fNameDriver">{driver?.strPlayer.substring(0, driver.strPlayer.indexOf(" "))}</h3>
+              <h3 className="lNameDriver">{driver?.strPlayer.substring(driver.strPlayer.indexOf(" "))}</h3>
+              <div className="driversNameBG1"></div>
+              <div className="driversNameBG2"></div>
+              <p className="driverNumber">{driver?.strNumber}</p>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 }
---
+++ src/drivers.ts
@@ -0,0 +1,24 @@
+const drivers: { firstName: string; lastName: string }[] = [
+  { firstName: "Max", lastName: "Verstappen" },
+  { firstName: "Lewis", lastName: "Hamilton" },
+  { firstName: "Sergio", lastName: "Perez" },
+  { firstName: "George", lastName: "Russell" },
+  { firstName: "Charles", lastName: "Leclerc" },
+  { firstName: "Carlos", lastName: "Sainz%Jr" },
+  { firstName: "Lando", lastName: "Norris" },
+  { firstName: "Oscar", lastName: "Piastri" },
+  { firstName: "Fernando", lastName: "Alonso" },
+  { firstName: "Lance", lastName: "Stroll" },
+  { firstName: "Esteban", lastName: "Ocon" },
+  { firstName: "Pierre", lastName: "Gasly" },
+  { firstName: "Alexander", lastName: "Albon" },
+  { firstName: "Logan", lastName: "Sargeant" },
+  { firstName: "Valtteri", lastName: "Bottas" },
+  { firstName: "Guanyu", lastName: "Zhou" },
+  { firstName: "Nico", lastName: "Hülkenberg" },
+  { firstName: "Kevin", lastName: "Magnussen" },
+  { firstName: "Yuki", lastName: "Tsunoda" },
+  { firstName: "Daniel", lastName: "Ricciardo" },
+];
+
+export { drivers };
