edd649d359d126f1da791b987dab2f356922a2c9
Jan Steikowski <j.steikowski@aol.com>
2023-10-24 12:54:34 +0700

    Add game describtion and styles

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -29,10 +29,25 @@ const StartScreen = () => {
     <>
       {loaded ? (
         <div className="flex flex-col justify-center items-center h-full">
-          <div className="flex flex-col bg-white p-2 rounded-2xl border-2 border-black">
-            <h2 className="text-lg">Welcome to F1 Memory Card Game!</h2>
-            <p>Choose the Difficulty</p>
-            <div className="flex justify-around gap-2    ">
+          <div className="flex flex-col bg-white pb-2 rounded-2xl border-2 border-black">
+            <h2
+              className="text-xl p-2 mb-2 text-white bg-asphalt rounded-t-xl shadow-lg"
+              style={{
+                textShadow: "1px 1px black",
+              }}
+            >
+              Welcome to F1 Memory Card Game!
+            </h2>
+            <p className="text-sm px-2 my-2 leading-7">Can you pick every driver from the 2023 Formula 1 season without clicking on one driver twice? Test your memory now!</p>
+            <p
+              className="my-2"
+              style={{
+                textShadow: "1px 1px light-gray",
+              }}
+            >
+              Choose the Difficulty
+            </p>
+            <div className="flex justify-around gap-2">
               <button>
                 <div className="tyreButton">
                   <div className="tyreText softTyre">
