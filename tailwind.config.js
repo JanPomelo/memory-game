c366cd6bfbc0222e8aab405de420acf1e9e262a0
Jan Steikowski <j.steikowski@aol.com>
2023-10-24 11:31:31 +0700

    Add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "british-green": "#004225",
+        "gulf-blue": "#007fff",
+        "gulf-orange": "#ff6600",
+        "merc-silver": "#000d2be",
+        "rb-blue": "#1e41ff",
+        "rb-red": "#d90d29",
+        "mclaren-orange": "#ff8700",
+        "ferrari-red": "#dc0000",
+        "pure-yellow": "#ffff00",
+      },
+    },
   },
   plugins: [],
 };
