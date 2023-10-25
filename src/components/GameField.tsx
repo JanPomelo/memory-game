c1cc9b6552f2a1575527a13567357621a3f87b92
Jan Steikowski <j.steikowski@aol.com>
2023-10-25 19:05:29 +0700

    implement score but does not count yet

--- src/components/GameField.tsx
+++ src/components/GameField.tsx
@@ -1,25 +1,12 @@
-//import { getDriverDetails } from "../apiCalls";
-//import { v4 } from "uuid";
 import React, { useState } from "react";
 import MemoryCard from "./MemoryCard";
 import GameFieldProps from "../interfaces/GameFieldProps";
 import { Driver } from "../types";
-//import { getDriverDetails } from "../apiCalls";
-/*
-async function getAllDetails(driverArr: { firstName: string; lastName: string }[]) {
-  const allDriverDetails: Driver[] = [];
-  for (let i = 0; i < driverArr.length; i++) {
-    const driverInfos = await getDriverDetails({ firstName: driverArr[i].firstName, lastName: driverArr[i].lastName });
-    driverInfos!.id = v4();
-    allDriverDetails.push(driverInfos!);
-  }
-  return allDriverDetails;
-}
-*/
 
 const GameField: React.FC<GameFieldProps> = ({ playArr, onStartPageClick }) => {
   const [gameArray, setGameArray] = useState(playArr);
   const [backTurn, setBackTurn] = useState("");
+  const [score, setScore] = useState(0);
   const newGameArr = [...gameArray];
 
   function shuffleArray(array: Driver[]) {
@@ -34,18 +21,19 @@ const GameField: React.FC<GameFieldProps> = ({ playArr, onStartPageClick }) => {
   const handleCardClick: React.MouseEventHandler<HTMLButtonElement> = (e: React.MouseEvent<HTMLButtonElement>) => {
     const button: HTMLButtonElement = e.target as HTMLButtonElement;
     const id: string = button.getAttribute("data-key") as string;
-    for (let i = 0; i < newGameArr.length; i++) {
-      if (id === newGameArr[i].id) {
-        newGameArr[i].clicked = true;
+      for (let i = 0; i < newGameArr.length; i++) {
+        if (id === newGameArr[i].id) {
+          newGameArr[i].clicked = true;
+        }
       }
-    }
-    button.blur();
-    setBackTurn("backTurn");
-    setTimeout(() => {
-      shuffleArray(newGameArr);
-      setGameArray(newGameArr);
-      setBackTurn("");
-    }, 1000);
+      button.blur();
+      setBackTurn("backTurn");
+      setTimeout(() => {
+        shuffleArray(newGameArr);
+        setGameArray(newGameArr);
+        setBackTurn("");
+        setScore(score + 1);
+      }, 1000);
   };
 
   return (
@@ -58,13 +46,9 @@ const GameField: React.FC<GameFieldProps> = ({ playArr, onStartPageClick }) => {
           Startpage
         </button>
         <div className="scores flex flex-col bg-white px-2 py-1 rounded-xl mx-auto sm:my-auto sm:mx-0">
-          <div className="flex">
-            <p>Current Score - </p>
-            <p></p>
-          </div>
-          <div className="flex justify-end">
-            <p>Best Score - </p>
-            <p></p>
+          <div className="flex gap-2">
+            <p>Score - </p>
+            <p>{score + '/' + newGameArr.length}</p>
           </div>
         </div>
       </div>
